71e3eadb05f1c4007b5e4b4d92c6980506050a7f
David Avalos <davalos@itparral.edu.mx>
2022-10-03 21:44:56 -0600

    feat: eslint funciona

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,18 +4,37 @@ module.exports = {
     node: true
   },
   extends: [
+    'eslint:recommended',
     'plugin:react/recommended',
-    'standard-with-typescript'
+    'standard-with-typescript',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'
   ],
   overrides: [
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    // ecmaVersion: 'latest',
+    // sourceType: 'module'
+    project: './tsconfig.json'
   },
   plugins: [
-    'react'
+    'react',
+    'react-hooks', 
+    '@typescript-eslint'
   ],
   rules: {
-  }
+    indent: ['error', 2, { SwitchCase: 1 }],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'no-empty-function': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 }
