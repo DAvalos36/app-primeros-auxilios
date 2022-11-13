78ffc662e65b5a2ed9492d6be9ec20d8a37af252
David Avalos <davalos@itparral.edu.mx>
2022-11-12 20:58:47 -0600

    feat: estilos y mejor carusel

--- screens/Pantalla1.tsx
+++ screens/Pantalla1.tsx
@@ -1,14 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet } from 'react-native'
+import { Text, View, Carousel, Image } from 'react-native-ui-lib'
 import React from 'react'
+
+const info = ['abc', 'dos', 'tres']
+
+const ElementoCarrusel = ({ texto }: { texto: string }): JSX.Element => {
+  return (
+    <View style={{ paddingHorizontal: 10 }}>
+      <Image
+        style={{ width: '100%', aspectRatio: 6 / 3, borderRadius: 20 }} source={{ uri: 'https://picsum.photos/500/300' }}
+        resizeMode='cover'
+      />
+      <Text text70>{texto}</Text>
+    </View>
+  )
+}
 
 const Pantalla1 = (): JSX.Element => {
   return (
-    <View>
-      <Text>Pantalla1</Text>
+    <View style={styles.container} >
+      <View style={styles.containerInicio}>
+        <Text text40>Inicio</Text>
+        <Carousel
+          autoplay
+          loop
+        >
+          {info.map((item, index) => (
+            <ElementoCarrusel key={index} texto={item} />
+          ))}
+        </Carousel>
+      </View>
+      <View style={styles.containerInteres}>
+        <Text text40>Interes</Text>
+      </View>
+
     </View>
   )
 }
 
 export default Pantalla1
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10
+  },
+  containerInicio: {
+    flex: 3,
+    borderRadius: 20
+  },
+  containerInteres: {
+    flex: 7
+  }
+})
