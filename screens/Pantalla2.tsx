1cc7cd68160b0e10ea2335aad1e2f667602d4d67
David Avalos <davalos@itparral.edu.mx>
2022-11-14 22:06:10 -0600

    TEMPORAL: pantalla en construccion con pruebas

--- screens/Pantalla2.tsx
+++ screens/Pantalla2.tsx
@@ -1,20 +1,33 @@
 import { StyleSheet } from 'react-native'
-import { View, Text } from 'react-native-ui-lib'
+import { View, Text, Button, Incubator, Colors } from 'react-native-ui-lib'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import React from 'react'
+import ElementoBusqueda from '../components/ElementoBusqueda'
+import ElementoPaso from '../components/ElementoPaso'
 
 const Pantalla2 = (): JSX.Element => {
   return (
-    <View useSafeArea style={styles.container}>
+    <SafeAreaView style={{ flex: 1 }} >
       {/* <Text>Pantalla2</Text> */}
       <View style={styles.superior}>
-        <Text text50>Pantalla2</Text>
-        <Text text50>Pantalla22</Text>
+        <View style={styles.superiorContenedorBarra} >
+          <Incubator.TextField
+            placeholder='Buscar'
+            style={ styles.barraBuscar }
+          />
+        </View>
+        <View style={styles.superiorContenedorBoton}>
+          <Button label='idk'/>
+        </View>
       </View>
 
       <View style={styles.contentido}>
-
+        <ElementoBusqueda nombre='asdasd'/>
+        <ElementoPaso item='asdasd' index={1} />
+        <ElementoPaso item='asdasd' index={2} />
+        <ElementoPaso item='laksdjlka' index={3} />
       </View>
-    </View>
+    </SafeAreaView>
   )
 }
 
@@ -25,9 +38,30 @@ const styles = StyleSheet.create({
     flex: 1
   },
   superior: {
-    flex: 2,
+    flex: 1,
     flexDirection: 'row',
-    justifyContent: 'flex-start'
+    justifyContent: 'space-between',
+    padding: 2
+  },
+  superiorContenedorBarra: {
+    flex: 7,
+    // alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 4
+    // backgroundColor: 'red'
+  },
+  barraBuscar: {
+    width: '100%',
+    borderWidth: 1,
+    borderColor: Colors.$outlinePrimary,
+    padding: 4,
+    borderRadius: 18,
+    backgroundColor: Colors.$backgroundDisabled
+  },
+  superiorContenedorBoton: {
+    flex: 3,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   contentido: {
     flex: 10
