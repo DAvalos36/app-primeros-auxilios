3fb0985669a58083ccf75e06ecef839cfabe3334
David Avalos <davalos@itparral.edu.mx>
2022-11-15 17:48:46 -0600

    wip: boton emergencias agreado

--- screens/PantallaProblema.tsx
+++ screens/PantallaProblema.tsx
@@ -1,5 +1,5 @@
-import { StyleSheet, ImageBackground } from 'react-native'
-import { View, Text, Colors } from 'react-native-ui-lib'
+import { StyleSheet, ImageBackground, Linking } from 'react-native'
+import { View, Text, Colors, Button } from 'react-native-ui-lib'
 import React from 'react'
 import { FlatList } from 'react-native-gesture-handler'
 
@@ -20,11 +20,13 @@ const PantallaProblema = (): JSX.Element => {
       <ImageBackground style={styles.imgCabecera} source={{ uri: 'https://picsum.photos/600/300' }} >
         <Text text30 color={Colors.primary} underline >Nombre problema:</Text>
       </ImageBackground>
-      <View style={{ flex: 1, padding: 20 }} >
+      <View style={{ flex: 1, padding: 20, paddingBottom: 10 }} >
         <Text text50>Introduccion:</Text>
         <Text text70>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse maiores qui fugiat repellendus asperiores ipsam natus perspiciatis molestias, omnis quam unde laudantium, officiis architecto ad, sunt commodi possimus ratione. Nulla.</Text>
         <Text text50 style={{ marginTop: 8 }}>Pasos a seguir:</Text>
         <FlatList data={['abac', 'efga', 'jkasdñaksdñlaskñdlasdklañsdlñaskñlasdkñladslkj', 'abc', 'efg', 'jkl', 'abc', 'efg', 'jkl', 'abc', 'efg', 'jkl']} renderItem={ElementoPaso} />
+        {/* Aqui abajo va a ir la variable peligro */}
+        {true && <Button label='Llamar a emergencias' backgroundColor={Colors.red30} style={{ marginTop: 10 }} />}
       </View>
     </View>
   )
