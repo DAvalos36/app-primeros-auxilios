98026bae5adf4be1722c7143fc271fe818975f5f
David Avalos <davalos@itparral.edu.mx>
2022-11-29 19:26:36 -0600

    feat: tipos de datos

--- types/PrimerosAuxilios.ts
+++ types/PrimerosAuxilios.ts
@@ -4,3 +4,19 @@ export interface Problema {
   contenido: string
   logoDir: string
 }
+
+export interface Caso {
+  titulo: string
+  introduccion: string
+  pasos: string[]
+  peligro: boolean
+  linkImagen: string
+  numeroEmergencia?: string
+}
+export interface Caso2 {
+  titulo: string
+  contenido: string
+  peligro: boolean
+  linkImagen: string
+  numeroEmergencia?: string
+}
