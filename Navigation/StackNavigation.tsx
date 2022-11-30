c6f95c87b7381325793f5aedcaab3d1a3ae0a461
David Avalos <davalos@itparral.edu.mx>
2022-11-29 18:34:39 -0600

    feat: tipado de navigation

--- Navigation/StackNavigation.tsx
+++ Navigation/StackNavigation.tsx
@@ -1,18 +1,29 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
+import type { StackNavigationProp } from '@react-navigation/stack'
+
+import { Caso2 } from '../types/PrimerosAuxilios'
 
 import Pantalla2 from '../screens/Pantalla2'
 import PantallaProblema2 from '../screens/PantallaProblema2'
 
-const Stack = createStackNavigator()
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+type StackNavigationParamList = {
+  Busqueda: undefined
+  Problema: Caso2
+}
+
+export type HStackNavigationProp = StackNavigationProp<StackNavigationParamList, 'Problema'>
+
+const Stack = createStackNavigator<StackNavigationParamList>()
 
 const StackNavigation = (): JSX.Element => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Pantalla 2" component={Pantalla2}
+      <Stack.Screen name="Busqueda" component={Pantalla2}
         options={{ headerShown: false }}
       />
-      <Stack.Screen name="Pantalla Pro" component={PantallaProblema2}
+      <Stack.Screen name="Problema" component={PantallaProblema2}
         options={{ headerShown: false }}
       />
     </Stack.Navigator>
